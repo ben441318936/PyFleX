f9e73190ba56eb28edd2fa3771e0a47c73bbfe39
jingbin <ben441318936@gmail.com>
2020-08-25 22:14:45 -0700

    update

--- bindings/examples/test_Custom.py
+++ bindings/examples/test_Custom.py
@@ -7,9 +7,12 @@ import time
 def rand_float(lo, hi):
     return np.random.rand() * (hi - lo) + lo
 
-exp = "exp31"
-time_step = 600
-num_particles = 3600
+exp = "exp36"
+time_step = 400
+num_particles = 2700
+
+scale = 5
+
 particle_path_prefix = "/home/jingbin/Documents/Github/PBF3D_taichi/viz_results/3D/new_MPC/{}/particles/".format(exp)    
 
 des_dir = "test_Custom_{}".format(exp)
@@ -28,7 +31,7 @@ pyflex.init()
 scene_params = np.array([0.2, 
                          num_particles,
                          0.0, 0.0, 0.0,
-                         3, 3, 3,
+                         15/scale, 20/scale, 15/scale,
                          1.0, 0.0, 0.0, 1.0,
                          1.0])
 pyflex.set_scene(10, scene_params, 0)
@@ -36,12 +39,15 @@ pyflex.set_scene(10, scene_params, 0)
 print("Scene Upper:", pyflex.get_scene_upper())
 print("Scene Lower:", pyflex.get_scene_lower())
 
-# Set all the particles to unseen locations
 pos = np.ones((num_particles,4))
-pos[:,0:3] = pos[:,0:3] * 20
+
+# Set all the particles to unseen locations
+def clear_pos():
+    pos[:,0:3] = np.ones((num_particles,3)) * 20
 
 # Function for putting particle positions into the array
 def load_pos(particles):
+    print(particles.shape[0])
     for i in range(particles.shape[0]):
         if i < num_particles:
             pos[i,0:3] = particles[i,0:3]
@@ -55,7 +61,8 @@ def load_pos(particles):
 for i in range(time_step):
     # Load in the particles
     path = particle_path_prefix+"frame_{}.npy".format(i)
-    particles = np.load(path)
+    particles = np.load(path) / scale
+    clear_pos()
     load_pos(particles)
     pyflex.set_positions(pos)
     # pyflex.step(capture=1, path=os.path.join(des_dir, "render_%04d.tga" % i))
